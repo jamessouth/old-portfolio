ddd6a73ca7f55df363ca585705acdae599d5c08b
jamessouth <jamesdanielsouth@gmail.com>
2018-09-15 00:44:30 -0500

    add linter

--- src/js/cube.js
+++ src/js/cube.js
@@ -12,119 +12,125 @@ import Project11 from '../images/project11.jpg';
 import Project12 from '../images/project12.jpg';
 
 
-
-
 const main = document.querySelector('main');
 const pb = document.querySelectorAll('.photo-cube');
 const cc = document.querySelectorAll('.cube-container');
 const hold = main.querySelector('.hold');
 const subhead = hold.querySelectorAll('.subhead:nth-of-type(odd)');
 const desc = hold.querySelectorAll('.subhead:nth-of-type(even)');
-const cubeLinks = document.querySelectorAll('div.cube-container div.photo-cube div a');
 
 
-
-
-
-
-let projects = ['random quotes', 'interactive form', 'tic tac toe', '15 puzzle generator', 'node web scraper', 'twitter interface', 'interactive video', 'accessibility refactor', 'web app dashboard', 'employee directory', 'flickr gallery', 'portfolio'];
+const projects = ['random quotes', 'interactive form', 'tic tac toe', '15 puzzle generator', 'node web scraper', 'twitter interface', 'interactive video', 'accessibility refactor', 'web app dashboard', 'employee directory', 'flickr gallery', 'portfolio'];
 const features = ['', '', '', 'AJAX webcam vue routing geolocation', '', 'Node express pug ES6 AJAX', '', '', 'AJAX vue-cli routing vuex Chart.js', 'React WAI-ARIA AJAX', 'React routing AJAX', 'Webpack babel eslint'];
 
 let isRotating = false;
 let xStart;
 let yStart;
-let xEnd;
-let yEnd;
 let whichPB = 0;
 
-let rotObj = {
-  0 : {
-    x : 0,
-    y : 0,
-    xs : 0,
-    ys : 0
+const rotObj = {
+  0: {
+    x: 0,
+    y: 0,
+    xs: 0,
+    ys: 0,
   },
-  1 : {
-    x : 0,
-    y : 0,
-    xs : 0,
-    ys : 0
-  }
+  1: {
+    x: 0,
+    y: 0,
+    xs: 0,
+    ys: 0,
+  },
 };
 
-let rando = () => Math.floor(Math.random() * 360) - 180;
+const rando = () => Math.floor(Math.random() * 360) - 180;
 
-let KEYFRAMES = () => ([
-  {transform: 'rotateX(0deg) rotateY(0deg)'},
-  {transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)`},
-  {transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)`},
-  {transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)`},
-  {transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)`},
-  {transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)`},
-  {transform: 'rotateX(0deg) rotateY(0deg)'}
+const KEYFRAMES = () => ([
+  { transform: 'rotateX(0deg) rotateY(0deg)' },
+  { transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)` },
+  { transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)` },
+  { transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)` },
+  { transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)` },
+  { transform: `rotateX(${rando()}deg) rotateY(${rando()}deg)` },
+  { transform: 'rotateX(0deg) rotateY(0deg)' },
 ]);
 
-let KEYTIMING = {duration: 6000, iterations: 1, delay: 1000, easing: 'linear'};
+const KEYTIMING = {
+  duration: 6000, iterations: 1, delay: 1000, easing: 'linear',
+};
 
 let no = 1;
-let spin, spin2;
+let spin;
+let spin2;
 
-const cubeProjectImgs = [null, Project1, Project2, Project3, Project4, Project5, Project6, Project7, Project8, Project9, Project10, Project11, Project12];
+const cubeProjectImgs = [
+  null,
+  Project1,
+  Project2,
+  Project3,
+  Project4,
+  Project5,
+  Project6,
+  Project7,
+  Project8,
+  Project9,
+  Project10,
+  Project11,
+  Project12,
+];
 
-async function buildCubes(){
-  let pic, pic2;
-  try{
+async function buildCubes() {
+  let pic;
+  let pic2;
+  try {
     pic = await fetch(cubeProjectImgs[no]);
     pic2 = await fetch(cubeProjectImgs[no + 6]);
     pic = await pic.blob();
     pic2 = await pic2.blob();
-    var objectURL = URL.createObjectURL(pic);
-    var objectURL2 = URL.createObjectURL(pic2);
+    const objectURL = URL.createObjectURL(pic);
+    const objectURL2 = URL.createObjectURL(pic2);
     pb[0].children[no - 1].style.backgroundImage = `url(${objectURL})`;
     pb[1].children[no - 1].style.backgroundImage = `url(${objectURL2})`;
-    no++;
-  }
-  catch(err){
+    no += 1;
+  } catch (err) {
     console.log('error ', err);
   }
 }
-animCubes().then(hoverCubes);
 
-function animCubes(){
-  spin = pb[0].animate(
-    KEYFRAMES(), KEYTIMING);
-  spin2 = pb[1].animate(
-    KEYFRAMES(), KEYTIMING);
+function hoverCubes() {
+  cc[0].animate([
+    { transform: 'translateY(0px)' },
+    { transform: 'translateY(10px)' },
+  ], { duration: 1000, iterations: 20, direction: 'alternate' });
+  cc[1].animate([
+    { transform: 'translateY(0px)' },
+    { transform: 'translateY(10px)' },
+  ], {
+    duration: 1000, iterations: 20, direction: 'alternate',
+  });
+}
 
-  let si = setInterval(() => {
-    console.log(spin.currentTime);
+function animCubes() {
+  spin = pb[0].animate(KEYFRAMES(), KEYTIMING);
+  spin2 = pb[1].animate(KEYFRAMES(), KEYTIMING);
+
+  const si = setInterval(() => {
     buildCubes();
-    if(spin.currentTime >= 4999){
+    if (spin.currentTime >= 4999) {
       clearInterval(si);
     }
   }, 1000);
 
   return Promise.all([spin.finished, spin2.finished]);
 }
+animCubes().then(hoverCubes);
 
 
-let hover, hover2;
-function hoverCubes(){
-  hover = cc[0].animate([
-    {transform: 'translateY(0px)'},
-    {transform: 'translateY(10px)'}
-  ], {duration: 1000, iterations: 20, direction: 'alternate'});
-  hover2 = cc[1].animate([
-    {transform: 'translateY(0px)'},
-    {transform: 'translateY(10px)'}
-  ], {duration: 1000, iterations: 20, direction: 'alternate'});
-}
-
-['mouseover', 'focus', 'touchstart'].forEach(evt => {
-  pb.forEach((x,i) => {
-    x.addEventListener(evt, e => {
-      if(e.target.tagName === 'A'){
-        let projNum = e.target.id;
+['mouseover', 'focus', 'touchstart'].forEach((evt) => {
+  pb.forEach((x, i) => {
+    x.addEventListener(evt, (e) => {
+      if (e.target.tagName === 'A') {
+        const projNum = e.target.id;
         subhead[i].textContent = projects[projNum - 1];
         desc[i].textContent = features[projNum - 1];
       }
@@ -133,9 +139,8 @@ function hoverCubes(){
 });
 
 
-
-function rotate(e){
-  if(!isRotating) return;
+function rotate(e) {
+  if (!isRotating) return;
   const xPos = (e.x || Math.floor(e.touches[0].clientX)) + window.scrollX;
   const yPos = (e.y || Math.floor(e.touches[0].clientY)) - 0 + window.scrollY;
   rotObj[whichPB].xs = xPos - xStart + rotObj[whichPB].x;
@@ -143,18 +148,22 @@ function rotate(e){
   pb[whichPB].style.transform = `rotateX(${-rotObj[whichPB].ys}deg) rotateY(${rotObj[whichPB].xs}deg)`;
 }
 
-function getCube(e){
-  const cubeZeroCtr = { x : (cc[0].offsetLeft + cc[0].offsetWidth/2),
-          y : (cc[0].offsetTop - 0 + cc[0].offsetHeight/2)
-        };
-  const cubeOneCtr = { x : (cc[1].offsetLeft + cc[1].offsetWidth/2),
-          y : (cc[1].offsetTop - 0 + cc[1].offsetHeight/2)
-        };
+function getCube(e) {
+  const cubeZeroCtr = {
+    x: (cc[0].offsetLeft + cc[0].offsetWidth / 2),
+    y: (cc[0].offsetTop - 0 + cc[0].offsetHeight / 2),
+  };
+  const cubeOneCtr = {
+    x: (cc[1].offsetLeft + cc[1].offsetWidth / 2),
+    y: (cc[1].offsetTop - 0 + cc[1].offsetHeight / 2),
+  };
   xStart = (e.x || Math.floor(e.touches[0].clientX)) + window.scrollX;
   yStart = (e.y || Math.floor(e.touches[0].clientY)) - 0 + window.scrollY;
-  const distFromCubeZero = Math.round(Math.sqrt(Math.pow(Math.abs(cubeZeroCtr.x - xStart),2) + Math.pow(Math.abs(cubeZeroCtr.y - yStart),2)));
-  const distFromCubeOne = Math.round(Math.sqrt(Math.pow(Math.abs(cubeOneCtr.x - xStart),2) + Math.pow(Math.abs(cubeOneCtr.y - yStart),2)));
-  if(distFromCubeZero <= distFromCubeOne){
+  const distFromCubeZero = Math.round(Math.sqrt((Math.abs(cubeZeroCtr.x - xStart) ** 2)
+  + (Math.abs(cubeZeroCtr.y - yStart) ** 2)));
+  const distFromCubeOne = Math.round(Math.sqrt((Math.abs(cubeOneCtr.x - xStart) ** 2)
+  + (Math.abs(cubeOneCtr.y - yStart) ** 2)));
+  if (distFromCubeZero <= distFromCubeOne) {
     whichPB = 0;
   } else {
     whichPB = 1;
@@ -164,23 +173,19 @@ function getCube(e){
 
 
 hold.addEventListener('mousedown', getCube);
-hold.addEventListener('touchstart', getCube, {passive: true});
+hold.addEventListener('touchstart', getCube, { passive: true });
 hold.addEventListener('mousemove', rotate);
-hold.addEventListener('touchmove', rotate, {passive: true});
+hold.addEventListener('touchmove', rotate, { passive: true });
 
 
-hold.addEventListener('touchmove', e => {
-  if(e.target.tagName === 'A'){
+hold.addEventListener('touchmove', (e) => {
+  if (e.target.tagName === 'A') {
     e.preventDefault();
   }
-}, {passive: false});
+}, { passive: false });
 
-function releaseCube(e){
+function releaseCube() {
   isRotating = false;
-  if(e.type[0] === 'm'){
-    xEnd = e.x + window.scrollX;
-    yEnd = e.y - 0 + window.scrollY;
-  }
   rotObj[whichPB].x = rotObj[whichPB].xs || 0;
   rotObj[whichPB].y = rotObj[whichPB].ys || 0;
 }
@@ -188,10 +193,4 @@ function releaseCube(e){
 
 hold.addEventListener('mouseup', releaseCube);
 hold.addEventListener('touchend', releaseCube);
-hold.addEventListener('mouseleave', (e) => {
-  isRotating = false;
-  xEnd = e.x + window.scrollX;
-  yEnd = e.y - 0 + window.scrollY;
-  rotObj[whichPB].x = rotObj[whichPB].xs || 0;
-  rotObj[whichPB].y = rotObj[whichPB].ys || 0;
-});
+hold.addEventListener('mouseleave', releaseCube);
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import "@babel/polyfill";
+import '@babel/polyfill';
 
-import './web-animations-next.min.js';
+import './web-animations-next.min';
 
 
 import '../css/styles.css';
@@ -24,25 +24,18 @@ import Project11GIF from '../images/p11.gif';
 
 const sizeInput = document.querySelector('.slider input');
 const sizeInputP = document.querySelector('.slider p');
-const sizeTrack = document.querySelector('.slider .track');
-
-
-const menuHolder = document.querySelector('ul');
-const options = document.querySelectorAll('li');
-const menuHold = document.querySelector('.menu-hold');
-
+const sizeTrack = document.querySelector('.track');
 
 const applyButton = document.querySelector('#apply');
-
 
 const check = document.querySelectorAll('.menu-hold input.check');
 const checkLabel = document.querySelectorAll('.menu-hold input.check + label');
 
 
 const headline = document.querySelector('h1');
-const seconds = document.querySelector('.menu-hold select');
-const secondsLabel = document.querySelector('.menu-hold label[for="sec"]');
-const secondsP = document.querySelector('li p.seconds');
+const seconds = document.querySelector('select');
+const secondsLabel = document.querySelector('label[for="sec"]');
+const secondsP = document.querySelector('p.seconds');
 
 const explode = document.querySelectorAll('.explode');
 const pb = document.querySelectorAll('.photo-cube');
@@ -50,10 +43,6 @@ let destroyFlag = false;
 
 const subhead = document.querySelectorAll('.subhead:nth-of-type(odd)');
 const desc = document.querySelectorAll('.subhead:nth-of-type(even)');
-
-
-
-
 
 check[0].checked = false;
 check[1].checked = false;
@@ -71,68 +60,68 @@ const proj9 = pb[1].querySelector('.right');
 const proj10 = pb[1].querySelector('.back');
 const proj11 = pb[1].querySelector('.top');
 
-check[0].addEventListener('change', function(e){
-  if(e.target.checked){
+check[0].addEventListener('change', (e) => {
+  if (e.target.checked) {
     checkLabel[0].textContent = 'GIFs!';
-    this.setAttribute('aria-checked', true);
+    check[0].setAttribute('aria-checked', true);
     proj7.style.backgroundImage = `url(${Project7GIF})`;
     proj9.style.backgroundImage = `url(${Project9GIF})`;
     proj10.style.backgroundImage = `url(${Project10GIF})`;
     proj11.style.backgroundImage = `url(${Project11GIF})`;
   } else {
     checkLabel[0].textContent = 'no GIFs';
-    this.setAttribute('aria-checked', false);
+    check[0].setAttribute('aria-checked', false);
     proj7.style.backgroundImage = `url(${Project7})`;
     proj9.style.backgroundImage = `url(${Project9})`;
     proj10.style.backgroundImage = `url(${Project10})`;
     proj11.style.backgroundImage = `url(${Project11})`;
   }
 });
-check[1].addEventListener('change', function(e){
-  if(e.target.checked){
+check[1].addEventListener('change', (e) => {
+  if (e.target.checked) {
     fetch(Explosion)
-    .then(res => {
-      return res.status !== 200 ? console.log('there was a problem fetching the resource. Status Code and Text: ' + res.status, res.statusText) : res.blob();
-    })
-    .then(res => {
-      var objectURL = URL.createObjectURL(res);
-      explode[0].src = objectURL;
-      explode[1].src = objectURL;
-    })
-    .catch(err => console.log('Fetch error: ', err));
+      .then(res => (res.status !== 200
+        ? console.log(`there was a problem fetching the resource. Status Code and Text: ${res.status}, ${res.statusText}`)
+        : res.blob()))
+      .then((res) => {
+        const objectURL = URL.createObjectURL(res);
+        explode[0].src = objectURL;
+        explode[1].src = objectURL;
+      })
+      .catch(err => console.log('Fetch error: ', err));
     checkLabel[1].textContent = 'DESTROY!';
-    this.setAttribute('aria-checked', true);
+    check[1].setAttribute('aria-checked', true);
     destroyFlag = true;
     seconds.style.display = 'block';
     secondsLabel.style.display = 'block';
     secondsP.style.display = 'block';
     seconds.removeAttribute('disabled');
     seconds.setAttribute('aria-disabled', false);
-    seconds.tabIndex = "1";
+    seconds.tabIndex = '1';
   } else {
     checkLabel[1].textContent = 'do not destroy';
-    this.setAttribute('aria-checked', false);
+    check[1].setAttribute('aria-checked', false);
     destroyFlag = false;
     seconds.style.display = 'none';
     secondsLabel.style.display = 'none';
     secondsP.style.display = 'none';
     seconds.setAttribute('disabled', '');
     seconds.setAttribute('aria-disabled', true);
-    seconds.tabIndex = "-1";
+    seconds.tabIndex = '-1';
   }
 });
 
 
-function destroyCube(cube){
+function destroyCube(cube) {
   cube.addEventListener('animationend', () => {
     headline.style.marginBottom = '0';
     cube.style.display = 'none';
     cube.parentNode.style.display = 'none';
-    for(let i = 0; i < 2; i++){
+    for (let i = 0; i < 2; i += 1) {
       subhead[i].style.display = 'none';
       desc[i].style.display = 'none';
     }
-    if(window.innerWidth < 768){
+    if (window.innerWidth < 768) {
       explode[0].style.display = 'block';
       explode[0].style.width = '560px';
       explode[0].style.height = '420px';
@@ -147,7 +136,7 @@ function destroyCube(cube){
 
 
   cube.classList.add('blowup');
-  window.setTimeout(() => {
+  setTimeout(() => {
     explode[0].style.display = 'none';
     explode[1].style.display = 'none';
   }, (seconds.value * 1000) + 5490);
@@ -159,23 +148,21 @@ function handleSelectUpdate() {
   this.children[this.value].setAttribute('aria-selected', true);
   const suffix = this.dataset.sizing;
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-  if(destroyFlag){
+  if (destroyFlag) {
     destroyCube(pb[0]);
     destroyCube(pb[1]);
   }
 }
 seconds.addEventListener('change', handleSelectUpdate);
-sizeInput.addEventListener('focus', function(e){
-  this.parentNode.style.border = "3px white solid";
+sizeInput.addEventListener('focus', () => {
+  sizeInput.parentNode.style.border = '3px white solid';
 });
-sizeInput.addEventListener('blur', function(e){
-  this.parentNode.style.border = "none";
+sizeInput.addEventListener('blur', () => {
+  sizeInput.parentNode.style.border = 'none';
 });
-
 
 
 function handleRangeUpdate() {
-  const suffix = this.dataset.sizing;
   const width = this.max - this.min;
   const perc = Math.floor(((this.value - width) / width) * 100) / 100;
 
@@ -187,6 +174,6 @@ function handleRangeUpdate() {
 sizeInput.addEventListener('change', handleRangeUpdate);
 sizeInput.addEventListener('mousemove', handleRangeUpdate);
 
-applyButton.addEventListener('click', e => {
+applyButton.addEventListener('click', () => {
   document.documentElement.style.setProperty('--size', `${sizeInput.value}px`);
 });
