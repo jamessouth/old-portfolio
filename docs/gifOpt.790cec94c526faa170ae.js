4a75617abe495698c2d04df5c4b657c7a3700270
jamessouth <jamesdanielsouth@gmail.com>
2018-10-23 20:55:36 -0500

    deploy

---
+++ docs/gifOpt.790cec94c526faa170ae.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{Jnpn:function(e,t,n){"use strict";n.r(t);n("rGqo");var c=document.querySelector("#gifs"),o=document.querySelector('label[for="gifs"]'),r=document.querySelector(".cube-container:last-of-type .photo-cube");function u(e,t){[0,2,3,4].forEach(function(t){r.children[t].setAttribute("gif",e)}),o.textContent=t}c.checked=!1,c.addEventListener("change",function(e){e.target.checked?u(!0,"GIFs!"):u(!1,"no GIFs")})}}]);
+//# sourceMappingURL=gifOpt.790cec94c526faa170ae.js.map
