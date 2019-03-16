cc6eec7172a7f4eb50d518577c983fcf05ed690d
jamessouth <jamesdanielsouth@gmail.com>
2019-03-15 21:42:39 -0500

    add sw map file to docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const ScriptExtHTMLWebpackPlugin = require('script-ext-html-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-const swVol = 'v8';
+const swVol = 'v9';
 
 module.exports = {
   mode: 'production', // development
