b4cf0603cefcce6c833278ec860c4ed156b32b49
jamessouth <jamesdanielsouth@gmail.com>
2019-06-10 19:32:29 -0500

    update use of clean wp plugin

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ module.exports = {
     },
   },
   plugins: [
-    new CleanWebpackPlugin(['docs'], { exclude: ['burst.min.js', 'worker.min.js'] }),
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['!burst.min.js', '!worker.min.js'] }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
