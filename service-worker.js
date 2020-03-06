2a42cfb9f9ed5cb2ee7d1e23b93c47705081e74a
jamessouth <jamesdanielsouth@gmail.com>
2020-03-05 23:21:26 -0600

    build

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,6 @@ precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
 
 addEventListener('message', e => {
-  if (e.data && e.data.type === 'VERSION_334') skipWaiting();
   if (e.data && e.data.type === 'SKIP_WAITING') skipWaiting();
 });
 
