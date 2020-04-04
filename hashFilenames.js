7fc2f3dff4b04ba9d8dd0af52bf324a0a2d267d1
jamessouth <jamesdanielsouth@gmail.com>
2020-04-03 23:31:38 -0500

    cont hash async

--- hashFilenames.js
+++ hashFilenames.js
@@ -3,36 +3,38 @@ const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
 
+function inner(data, direc) {
+  const paths = data.match(/\.\.?\/(\w+\/)?\w+\.(css|m?js|png|jpg|pdf)/g);
+  // console.log(`paths for ${file}: `, paths);
+  
+  paths && paths.forEach(async p => {
+    const {dir, base, ext} = path.parse(p);
+    if (['.png', '.jpg', '.pdf'].includes(ext)) {
+      const fileToHash = path.resolve('docs', direc, p);
+      console.log('image: ', fileToHash);
+      await hashFile(fileToHash).then(x => console.log('newname: ', x));
+    } else {
+      console.log('file: ', p, dir, direc);
+      if (dir == '.') {
+        console.log('first: ', direc, base);
+        hash(direc, base);
+      } else {
+        console.log('2nd: ', dir, base);
+        hash(dir, base);
+      }
+    }
+  });
 
+}
 
 function hash(direc, file) {
   console.log('filepath: ', path.resolve('docs', direc, file));
   const data = fs.readFileSync(path.resolve('docs', direc, file), {encoding: 'utf8'});
   
   
-  function inner(data) {
-    const paths = data.match(/\.\.?\/(\w+\/)?\w+\.(css|m?js|png|jpg|pdf)/g);
-    console.log(`paths for ${file}: `, paths);
-    
-    paths && paths.forEach(p => {
-      const {dir, base, ext} = path.parse(p);
-      if (['.png', '.jpg', '.pdf'].includes(ext)) {
-        console.log('image: ', p);
-      } else {
-        console.log('file: ', p, dir, direc);
-        if (dir == '.') {
-          console.log('first: ', direc, base);
-          hash(direc, base);
-        } else {
-          console.log('2nd: ', dir, base);
-          hash(dir, base);
-        }
-      }
-    });
 
-  }
 
-  inner(data);
+  inner(data, direc);
 
 }
 
@@ -65,7 +67,7 @@ hash(process.argv[2], process.argv[3]);
 //   return results;
 // }
 
-// // getFiles function adapted from https://stackoverflow.com/a/45130990
+// getFiles function adapted from https://stackoverflow.com/a/45130990
 // async function* getFiles(inputDir) {
 //   const dir = await fs.promises.readdir(inputDir, { withFileTypes: true });
 //   for (const dirent of dir) {
@@ -74,9 +76,9 @@ hash(process.argv[2], process.argv[3]);
 //       yield* getFiles(filepath);
 //     }
 //     if (dirent.isFile() && !['index.html', 'manifest.webmanifest'].includes(dirent.name)) {
-//         const file = fs.createReadStream(filepath);
-//         const {name, ext} = path.parse(filepath);
-//         const hash = crypto.createHash('md4');
+        
+        
+        
 //         const newname = await hashFile(hash, file, name, ext, inputDir, filepath);
 //         const oldname = name + ext;
 //         yield [new RegExp(oldname, 'g'), newname];
@@ -84,22 +86,25 @@ hash(process.argv[2], process.argv[3]);
 //   }
 // }
 
-// async function hashFile(hash, file, name, ext, inputDir, filepath) {
-//     return new Promise(res => {
-//         file.on('readable', () => {
-//             const data = file.read();
-//             if (data) {
-//                 hash.update(data);
-//             } else {
-//                 const dig = hash.digest('hex');
-//                 const newname = name + '.' + dig + ext;
-//                 const newpath = path.resolve(inputDir, newname);
-//                 fs.renameSync(filepath, newpath);
-//                 res(newname);
-//             }
-//         });
-//     });
-// }
+async function hashFile(filepath) {
+    const file = fs.createReadStream(filepath);
+    const hash = crypto.createHash('md5');
+    const {dir, name, ext} = path.parse(filepath);
+    return new Promise(res => {
+        file.on('readable', () => {
+            const data = file.read();
+            if (data) {
+                hash.update(data);
+            } else {
+                const dig = hash.digest('hex');
+                const newname = name + '.' + dig + ext;
+                const newpath = path.resolve(dir, newname);
+                fs.renameSync(filepath, newpath);
+                res(newname);
+            }
+        });
+    });
+}
 
 // async function createLists(dir, list) {
 //   // for await (const f of getFiles(dir)) {
