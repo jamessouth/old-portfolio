8d0d18eedffe160351fe417e49a76cf6ef10ac66
jamessouth <jamesdanielsouth@gmail.com>
2020-05-01 00:49:24 -0500

    lint dep check

--- dep-check.js
+++ dep-check.js
@@ -1,97 +1,83 @@
 const https = require('https');
 const { Transform } = require('stream');
 
-
-async function go(arr, pkg) {
-    return new Promise(async res => {
-        let r;
-        let i = 0;
-        do {
-            r = await getData(i, [], pkg);
-            arr.push(r);
-            i += 36;
-        } while (r.length > 0);
-        res(arr);
-    });
-}
-
-async function run(arr) {
-    return await Promise.all(arr.map(p => go([], p)));
-}
-
-run(process.argv.slice(2))
-.then(e => e.map(y => y.flat()))
-.then(s => {
-    console.log('gggg: ', red(s));
+const breakIntoSections = () => new Transform({
+  transform(ch, _, cb) {
+    const rows = ((this.partialRow || '') + ch.toString()).split(/<\/section>/);
+    this.partialRow = rows.pop(); // eslint-disable-next-line no-restricted-syntax
+    for (const row of rows) {
+      this.push(`${row}\n`);
+    }
+    cb();
+  },
+  flush(cb) {
+    this.push(this.partialRow || '');
+    cb();
+  },
 });
 
-async function getData(i, arr, pkg) {
-    return new Promise(res => {
-        setTimeout(() => {
-            console.log('fired off: ', pkg, Date.now());
-            https.get(`https://www.npmjs.com/browse/depended/${pkg}?offset=${i}`, chunks =>
-            chunks
-            .pipe(filt())
-            .pipe(filt2())
-            .pipe(filt3(arr, res)));
-        }, 3200);
-    });
+const getH3s = () => new Transform({
+  transform(ch, _, cb) {
+    const str = ch.toString();
+    if (/<h3.+h3>/g.test(str)) {
+      const newstr = str.match(/<h3.+h3>/g, '');
+      this.push(`${newstr}\n`);
+    }
+    cb();
+  },
+});
+
+const getPackageNames = (arr, resolve) => new Transform({
+  transform(ch, _, cb) {
+    const str = ch.toString();
+    const newstr = str.match(/>.+(?=<\/h3)/g, '')[0].slice(1);
+    if (!newstr.startsWith('Help')) {
+      arr.push(newstr);
+    }
+    cb();
+  },
+  flush(cb) {
+    resolve(arr);
+    cb();
+  },
+});
+
+async function getData(offset, arr, pkg) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      https.get(`https://www.npmjs.com/browse/depended/${pkg}?offset=${offset}`, (chunks) => chunks
+        .pipe(breakIntoSections())
+        .pipe(getH3s())
+        .pipe(getPackageNames(arr, resolve)));
+    }, process.argv.slice(2).length * 1000);
+  });
 }
 
-
-const filt2 = () => {
-    return new Transform({
-        transform(ch, _, cb) {
-            let newstr;
-            const str = ch.toString();
-            if (/<h3.+h3>/g.test(str)) {
-                newstr = str.match(/<h3.+h3>/g, '');
-                this.push(newstr + "\n")
-            }
-            cb();
-        }
-    });
-};
-
-const filt3 = (arr, r) => {
-    return new Transform({
-        transform(ch, _, cb) {
-            const str = ch.toString();
-            const newstr = str.match(/>.+(?=<\/h3)/g, '')[0].slice(1);
-            if (!newstr.startsWith("Help")) {
-                arr.push(newstr);
-            }
-            cb();
-        },
-        flush(cb) {
-            r(arr);
-            cb();
-          }
-    });
-};
-
-const filt = () => {
-    return new Transform({
-        transform(ch, enc, cb) {
-            const rows = ((this.partialRow || '') + ch.toString()).split(/<\/section>/);
-            this.partialRow = rows.pop();
-            for (let row of rows) {
-              this.push(row + "\n");
-            }
-            cb();
-          },
-          flush(cb) {
-            this.push(this.partialRow || '');
-            cb();
-          }
-    });
-};
-
-function red(arr) {
-    return arr.reduce((acc, cur) => {
-        return acc.filter(e => cur.includes(e));
-    });
+async function getDepArray(arr, pkg) { // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve) => {
+    let data;
+    let offset = 0;
+    do { // eslint-disable-next-line no-await-in-loop
+      data = await getData(offset, [], pkg);
+      arr.push(data);
+      offset += 36;
+    } while (data.length > 0);
+    resolve(arr);
+  });
 }
+
+async function main(arr) {
+  return Promise.all(arr.map((pkg) => getDepArray([], pkg)));
+}
+
+function getSharedDeps(arr) {
+  return arr.reduce((acc, cur) => acc.filter((e) => cur.includes(e)));
+}
+
+main(process.argv.slice(2))
+  .then((arr) => arr.map((subArr) => subArr.flat()))
+  .then((results) => console.log('results: ', getSharedDeps(results)));
+
 
 // deps common to terser, html-minifier, postcss:
 
