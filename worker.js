cb7e8dddcb90396e1c03b8d4d03bb0ff03672fc7
jamessouth <jamesdanielsouth@gmail.com>
2020-05-24 15:29:45 -0500

    build style string

--- worker.js
+++ worker.js
@@ -1,14 +1,12 @@
-const colors = [
-  {"typescript": "#2b7489"},
-  {"shell": "#89e051"},
-  {"go": "#00ADD8"},
-  {"vue": "#2c3e50"},
-  {"html": "#e34c26"},
-  {"css": "#563d7c"},
-  {"javascript": "#f1e05a"},
-];
-
-
+const colors = {
+  "typescript": "#2b7489",
+  "shell": "#89e051",
+  "go": "#00ADD8",
+  "vue": "#2c3e50",
+  "html": "#e34c26",
+  "css": "#563d7c",
+  "javascript": "#f1e05a",
+};
 
 const options = {
   headers: {
@@ -17,26 +15,37 @@ const options = {
   }
 };
 
-
-
-
-
-
 addEventListener('message', (e) => { // eslint-disable-line
   fetch(`https://api.github.com/repos/jamessouth/${e.data.repoName}/languages`, options)
   .then(res => res.json())
   .then(res => {
     const sum = Object.values(res).reduce((a, b) => a += b);
-    console.log(': ', res, sum);
+    const sorted = Object.entries(res).sort((a, b) => b[1] - a[1]);
+    let ans = 'linear-gradient(to right', tot = 0;
+    sorted.forEach(([lang, count], i) => {
+      const pct = Math.floor((count/sum) * 100000) / 1000;
+      if (i == 0) {
+        ans += `, ${colors[lang.toLowerCase()]} ${pct}%`;
+      } else if (i == sorted.length - 1) {
+        ans += `, ${colors[lang.toLowerCase()]} ${tot}%`;
+      } else {
+        ans += `, ${colors[lang.toLowerCase()]} ${tot}% ${pct + tot}%`;
+      }
+      
+      tot += pct;
+      console.log('ddd: ', lang, count);
+    });
+
+
+    
+
+    
+    console.log(': ', ans);
   })
   .catch(e => console.error('eee: ', e));
   
 
 
-  // let a = Math.floor(Math.random()*1000)/10, b = Math.floor(Math.random()*1000)/10, c = Math.floor(Math.random()*1000)/10;
-  // let tot = a+b+c;
-  // let x = Math.floor(a/tot*1000)/10, y = Math.floor(b/tot*1000)/10, z = Math.floor(c/tot*1000)/10;
-  // console.log('ddd: ', x, y, z, x+z+y);
   // const data = {
   //   id: e.data.id,
   //   bar: `linear-gradient(to right, red ${x}%, orange ${x}% ${parseFloat((x+y).toFixed(2), 10)}%, blue ${parseFloat((x+y).toFixed(2), 10)}%)`,
